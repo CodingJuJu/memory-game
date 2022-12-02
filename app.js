ed72b2307882b6d0f1c31b893dcf3f352d2503cb
CodingJuJu <junaidalexander76@gmail.com>
2022-12-02 11:33:37 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const cardArray = [
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+
+  {
+    name: 'cheeseburger.png',
+    img: 'images/cheeseburger.png',
+  },
+
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png',
+  },
+  {
+    name: 'fries',
+    img: 'images/fries.png',
+  },
+  {
+    name: '',
+    img: 'images/fries.png',
+  }
+]
